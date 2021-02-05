b8dafe1bf63dd80e1f83a40cf0a62fb730c07abf
mambax7 <mambax7@gmail.com>
2021-02-05 05:45:17 -0500

    english

---
+++ language/english/main.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Jill Equipment module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license    http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package    Jill Equipment
+ * @since      2.5
+ * @author     Jill(tnjaile@gmail.com)
+ * @version    $Id $
+ **/
+
+xoops_loadLanguage('main', 'tadtools');
+define('_TAD_NEED_TADTOOLS','You need tadtools module, go to <a href="http://campus-xoops.tn.edu.tw/modules/tad_modules/index.php?module_sn=1" target="_blank" >XOOPS easy frame</a> download.');
+
+define('_MD_JILLEQUIPMENT_BACK','Return');
+define('_MD_JILLEQUIPMENT_IS_ENABLE','Is it enabled?');
+define('_MD_JILLEQUIPMENT_BSN','Appointment Number');
+define('_MD_JILLEQUIPMENT_BUID','Booker');
+define('_MD_JILLEQUIPMENT_BOOKING_TIME','Appointment time');
+define('_MD_JILLEQUIPMENT_BOOKING_CONTENT','Reasons for appointment');
+define('_MD_JILLEQUIPMENT_PLACE','Place of use');
+define('_MD_JILLEQUIPMENT_START','Start date');
+define('_MD_JILLEQUIPMENT_END','Scheduled return date');
+define('_MD_JILLEQUIPMENT_BDATE','Appointment date');
+define('_MD_JILLEQUIPMENT_TSN','Time period number');
+define('_MD_JILLEQUIPMENT_STATUS','Status');
+define('_MD_JILLEQUIPMENT_APPROVER','reviewer');
+define('_MD_JILLEQUIPMENT_LOAN_DATE','Lending time');
+define('_MD_JILLEQUIPMENT_RETURN_DATE','Actual return time');
+define('_MD_JILLEQUIPMENT_WEEK','week');
